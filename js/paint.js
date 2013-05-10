12de3a8a6b0791cb1b38fb5474402fa21459f781
Nilesh <nil.1511@rediffmail.com>
2013-05-10 19:14:13 +0530

    Rect add

--- js/paint.js
+++ js/paint.js
@@ -1,19 +1,45 @@
 $(function(){
-var x1,y1,x2,y2l;
-    var can = $("canvas")[0].getContext("2d");
-$("#paint").mousedown(function(e){
-can.beginPath();
-x1=e.clientX;
-y1=e.clientY;
-can.moveTo(x1, y1);
+var x1,y1,x2,y2l,d=0;
+var can = $("canvas")[0].getContext("2d");
+$("button").click(function(e){
+//console.log(e.currentTarget.textContent);
+switch (e.currentTarget.textContent){
+case "Line":
+d=0;
+break;
+case "Rect":
+d=1;
+break;
+case "Circle":
+d=2;
+break;
+case "Square":
+d=3;
+break;
+}
 
+});
+$("#paint").mousedown(function(e){
+console.log(e);
+x1=e.pageX - this.offsetLeft;
+y1=e.pageY - this.offsetTop;
+can.beginPath();
+can.moveTo(x1, y1);
 });
 $("#paint").mouseup(function(e){
-x2=e.clientX;
-y2=e.clientY;
-can.lineTo(x2, y2);
+x2=e.pageX - this.offsetLeft;
+y2=e.pageY - this.offsetTop;
 
+switch (d){
+case 0:
+{can.lineTo(x2, y2);
+break;
+}
+case 1:{
+can.rect(x1,y1,Math.abs(x2-x1),Math.abs(y2-y1));
+break;
+}
+}
 can.stroke();
 });
-
 });
